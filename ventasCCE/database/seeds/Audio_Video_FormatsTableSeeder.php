836347788f73380e83920e9fef5716f2b41ac9f4
Wocking Bash <josue.garrido@epn.edu.ec>
2020-10-05 19:52:20 -0500

    seeder

---
+++ ventasCCE/database/seeds/Audio_Video_FormatsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Audio_Video_FormatsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
