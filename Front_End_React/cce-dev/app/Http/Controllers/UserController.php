f44794696331573317b60c044f22a87fe141aaaf
XJaramillo <francisco.jaramillo01@epn.edu.ec>
2020-11-10 16:27:22 -0500

    “Front_End_React”

---
+++ Front_End_React/cce-dev/app/Http/Controllers/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Writer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Resources\User as UserResource;
+use Tymon\JWTAuth\JWTGuard;
+
+
+class UserController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['message' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'could_not_create_token'], 500);
+        }
+        $user = JWTAuth::user();
+
+        return response()->json(compact('token', 'user'));
+        // for httpOnly cookie
+        //->withCookie(
+        //                'token',
+        //                auth()->getToken()->get(),
+        //                config('jwt.ttl'),
+        //                '/'
+        //            );
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'editorial' => 'required|string',
+            'short_bio' => 'required|string',
+        ]);
+
+        $writer = Writer::create([
+            'editorial' => $request->get('editorial'),
+            'short_bio' => $request->get('short_bio'),
+        ]);
+
+        $writer->user()->create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),]);
+
+        $user = $writer->user;
+
+        $token = JWTAuth::fromUser($writer->user);
+
+        return response()->json(new UserResource($user, $token), 201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['message' => 'user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['message' => 'token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['message' => 'token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['message' => 'token_absent'], $e->getStatusCode());
+        }
+        return response()->json(new UserResource($user), 200);
+    }
+
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json([
+                "status" => "success",
+                "message" => "User successfully logged out."
+            ], 200);
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(["message" => "No se pudo cerrar la sesión."], 500);
+        }
+    }
+}
