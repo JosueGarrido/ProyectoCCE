9efc94a879da6641957172791ed3de417c5b2e41
Bran <garciabrandonarevalo@gmail.com>
2021-02-03 23:59:08 -0500

    relacion follower

--- ventasCCE/app/Follower.php
+++ ventasCCE/app/Follower.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Follower extends Model
 {
-    //
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($follower) {
+            $follower->user_id = Auth::id();
+        });
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
